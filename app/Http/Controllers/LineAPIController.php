e78d4335be4ff0752bd593ab9afb809ac9986f26
Hhidek2211 <hhidek2211@gmail.com>
2023-09-19 23:54:23 +0900

    change test method to LineAPIcontroller

--- app/Http/Controllers/LineAPIController.php
+++ app/Http/Controllers/LineAPIController.php
@@ -10,14 +10,8 @@ class LineAPIController extends Controller
 {
     public function index(Request $request LINEBot $bot): JsonResponse {
     
-    $httpClient = new CurlHTTPClient($_ENV['LINE_ACCESS_TOKEN']);
-    $bot = new LINEBot($httpClient, ['channelSecret' => $_ENV['LINE_CHANNEL_SECRET']]);
-        
-    $request->collect('events')->each(function ($event) use ($bot) {
-        $bot->replyText($event['replyToken'], $event['message']['text']);
-    });
+    return response()->json('success');
     
-    return 'ok!';
     }
     
 }
