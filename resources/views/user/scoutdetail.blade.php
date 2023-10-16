447112cfaab051ce76665f26cb8d83d794eb2e30
Hhidek2211 <hhidek2211@gmail.com>
2023-10-16 21:44:04 +0900

    adjust layout about scout

--- resources/views/user/scoutdetail.blade.php
+++ resources/views/user/scoutdetail.blade.php
@@ -1,28 +1,71 @@
 <x-app-layout>
   <!DOCTYPE html>
     <body>
-     <h1>{{ $band->name }}さんからのスカウト</h1>
-     <h2>バンドについての情報</h2>
-     <p>バンドメンバー</p>
-      @foreach( $band->user_profiles as $member)
-        <p>{{ $member->name }}</p>
-      @endforeach
-     <br>
-     <p>バンド紹介</p>
-      <p>{{ $band->introduction }}</p>
-     <br>
-     <h2>スカウトの情報</h2>
-      <p>タイトル：{{ $scout->title }}</p>
-      <p>依頼楽器：{{ $scout->instrument->name_abb }}</p>
-      <p>メッセージ：</p>
-      <p>{{ $scout->message }}</p>
+        <div class="w-11/12 md:w-1/2 text-center text-2xl text-white bg-blue-300 border-white border-2 rounded-full mx-auto my-4">
+            スカウトの詳細
+        </div>
+        <div class="container w-full md:w-3/5 mx-auto text-center border border-4 border-gray-300 rounded-xl bg-white px-1 md:px-4 py-3 mb-12">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center space-y-2 md:space-y-0 md:space-x-2">
+                <div id="bandInfomation" class="container border border-4 border-gray-300 rounded-xl w-full mx-auto px-1 pt-1 pb-2 space-y-1 text-left">
+                    <div class="w-11/12 md:w-2/3 text-center text-xl text-white bg-blue-500 border-white border-2 rounded-full mx-auto">
+                        バンドの情報
+                    </div>
+                    <div id="name" class="">
+                        <p class="font-lg">募集者のバンド名</p>
+                        <p class="text-xl">{{ $band->name }}</p>
+                    </div>
+                    <div id="member">
+                        <p class="font-lg">メンバー</p>
+                        <div id="memberTable" class="flex flex-col">
+                            <div class="-m-1.5 overflow-x-auto">
+                                <div class="p-1.5 min-w-full inline-block align-middle">
+                                    <div class="border border-2 border-gray-300 rounded-lg overflow-hidden">
+                                        <table class="min-w-full divide-y divide-gray-400">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col" class="px-3 py-1 text-left text-sm text-gray-500 uppercase">名前</th>
+                                                    <th scope="col" class="px-3 py-1 text-left text-sm text-gray-500 uppercase">学年</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody class="divide-y divide-gray-200">
+                                                @foreach($band->user_profiles as $member)
+                                                    <tr>
+                                                        <td class="px-3 py-1 whitespace-nowrap text-md">{{ $member->name }}</td>
+                                                        <td class="px-3 py-1 whitespace-nowrap text-md">{{ $member->grade }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="introduction" class="mb-1">
+                        <p class="font-lg">バンド紹介文</p>
+                        <p class="px-0.5 border-2 border-gray-300 rounded-lg whitespace-pre-line">{{ $band->introduction }}</p>
+                    </div>
+                </div>
+          
+            <div id="scoutInfo" class="container border border-4 border-gray-300 rounded-xl w-full mx-auto px-1 pt-1 pb-2 space-y-1 text-left">
+                <div class="w-11/12 md:w-2/3 text-center text-xl text-white bg-blue-500 border-white border-2 rounded-full mx-auto">
+                    スカウトの情報
+                </div>
+                <p>タイトル</p>
+                <p class="text-xl">{{ $scout->title }}</p>
+                <p>依頼楽器：{{ $scout->instrument->name_jp }}</p>
+                <p>メッセージ</p>
+                <p>{{ $scout->message }}</p>
+            </div>
+        </div>
       
-      <div class="approve">
-        <form action="{{ route('scout_approve', ['user'=> $user->id, 'scout'=> $scout->id]) }}" method="POST">
-          @csrf
-          @method('PUT')
-          <input type="submit" value="スカウトを承認する">
-      </div>
+            <div id="approve" class="mt-2">
+                <form action="{{ route('scout_approve', ['user'=> $user->id, 'scout'=> $scout->id]) }}" method="POST">
+                @csrf
+                @method('PUT')
+                    <input type="submit" value="スカウトを承認する">
+                </form>
+            </div>
     </body>
 </html>
 
--- resources/views/user/scoutlist.blade.php
+++ resources/views/user/scoutlist.blade.php
@@ -1,11 +1,24 @@
 <x-app-layout>
   <!DOCTYPE html>
     <body>
-     <h1>あなたへのスカウト一覧</h1>
-     @foreach( $scouts as $scout )
-        <a href="{{ route('scout_detail', ['user'=> $user->id, 'scout'=> $scout->id ]) }}">{{ $scout->band_profile->name }}さん</a>
-        <p>依頼楽器：{{ $scout->instrument->name_abb }}</p>
-     @endforeach
+        <div class="w-11/12 md:w-1/2 text-center text-2xl text-white bg-blue-300 border-white border-2 rounded-full mx-auto my-4">
+            あなたへのスカウト一覧
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 justify-around w-10/12 md:w-3/5 mx-auto">
+            @foreach( $scouts as $scout )
+                <a class="container border border-4 border-gray-300 rounded-xl bg-white w-full h-56 mx-auto px-2 py-2" href="{{ route('scout_detail', ['user'=> $user->id, 'scout'=> $scout->id ]) }}">
+                    <div class="text-center display:inline-block ">
+                        <p class="text-xl">{{ $scout->band_profile->name }}
+                        <span class="text-sm">さん</span></p>
+                    </div>
+                    <p>タイトル：</p>
+                    <p>{{ $scout->title }}</p>
+                    <p>依頼楽器：{{ $scout->instrument->name_abb }}</p>
+                    <p>メッセージ：</p>
+                    <p>{{ $scout->message }}</p>
+                </a>
+            @endforeach
+        </div>
     </body>
 </html>
 
